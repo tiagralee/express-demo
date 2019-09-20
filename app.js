33caba820623526ce07c72acfc5f74b8646280f4
admin-at-371541600170 <alex.li@carsales.com.au>
2019-09-20 23:17:55 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000
+
+
+app.use('/', function(req, res, next) {
+    console.log('middleware1')
+    next()
+})
+
+function logMessage(req, res, next) {
+    console.log('middleware2');
+    next();
+}
+
+app.get('/', logMessage, (req, res) => res.send('root'))
+app.get('/hello', (req, res) => {
+    console.log('request comming in');
+    res.send('world');
+})
+
+app.listen(port, ()=> {console.log('application started at 3000...')} );
